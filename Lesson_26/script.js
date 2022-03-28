ea93804e1b59491b8971be391eb7c90cd95e6f48
FirstGithubAcc <nazarovj891@gmail.com>
2022-03-28 17:59:41 +0500

    Dynamic typing | Lesson 26

---
+++ Lesson_26/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// To string
+
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2)
+console.log(typeof(null + ''));
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 16 + 'px';
+
+// To number
+
+// 1)
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(typeof(+'5'));
+
+// 3)
+console.log(typeof(parseInt("15px", 10)));
+
+let answer = +prompt("Hello", "");
+
+// To boolean 
+
+// 0, '', null, undefined, NaN; -> always false
+
+// 1)
+let switcher = null;
+
+// if (switcher) {
+//     console.log("Working...");
+// }
+
+switcher = 1;
+
+if (switcher) {
+    console.log("Working...");
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
