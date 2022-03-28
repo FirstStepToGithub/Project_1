38b3f6feaa9e9f8078e8ed116060b87049dd74fc
FirstGithubAcc <nazarovj891@gmail.com>
2022-03-28 20:19:47 +0500

    Job interwiew tasks

---
+++ Lesson_27/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+
+// 1)
+// let x = 5;
+// alert(x++);
+
+// 2)
+// [] + false - null + true
+// console.log([] + false - null + true);
+
+
+// 3)
+// let y = 1; 
+
+// let x = y = 2;
+
+// console.log(x);
+
+
+// 4)
+// console.log([] + 1 + 2);
+
+// 5)
+// alert("1"[0]);
+
+// 6)
+// console.log(2 && 1 && null && 0 && undefined);
+// && -> И = and - запинается на лжи (false)
+// || -> ИЛИ = or - запинается на правде (true)
+
+// 7)
+// !!(a && b) and (a && b) - есть ли разница между выражениями?
+// console.log(!!(1 && 2) === (1 && 2)); // !! -> два НЕ превращает в Boolean ое выражение
+ 
+// 8)
+// alert( null || 2 && 3 || 4 );
+
+// 9)
+// let a = [1, 2, 3],
+//     b = [1, 2, 3];
+// console.log( a == b );
+
+// 10)
+// alert(+"Infinity");
+
+// 11)
+// console.log("Ежик" > "яблоко");
+
+// 12)
+console.log( 0 || "" || 2 || undefined || true || false );
